14ee8cea33abba18ce32e761f5ea6acf1289576a
Kihong_Kim <rlghd0157gmail.compwd>
2025-01-16 20:13:09 +0900

    lv6 완성

--- src/main/java/com/example/lv6/Cart.java
+++ src/main/java/com/example/lv6/Cart.java
@@ -14,11 +14,34 @@ public class Cart {
         this.menuItemList = menuItemList;
     }
 
+    //장바구니 목록 반환
     public List<MenuItem> getMenuItemList() {
         return menuItemList;
+    }
+    //장바구니 비우기
+    public void clear() {
+        menuItemList.clear();
+    }
+    public void addItem(MenuItem item) {
+        if (item != null) {
+            menuItemList.add(item);
+        }
     }
 
     public void setMenuItemList(List<MenuItem> menuItemList) {
         this.menuItemList = menuItemList;
+    }
+
+    //장바구니 안에 물건 가격 총합
+    public double getTotalPrice() {
+        double totalPrice = 0;
+        for (int i = 0; i < menuItemList.size(); i++) {
+            totalPrice += menuItemList.get(i).getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void showcart() {
+        System.out.println();
     }
 }
